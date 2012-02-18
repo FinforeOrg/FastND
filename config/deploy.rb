58dbb8ee24c58da1034db7f7f18c7188ae935ba8
FastND Developer <staging@FFnetTest.(none)>
2012-02-18 07:48:59 +0000

    fixing rbenv issue in capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,6 @@
 require 'bundler/capistrano'
-set :application, "api.finfore.net"
+set :application, "FastND"
+set :domain, "api.finfore.net"
 set :repository,  "git@github.com:FinforeOrg/FastND.git"
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
@@ -10,14 +11,17 @@ set :deploy_to, "/var/www/FastND"
 # we don't need this, turning this to true will makes capistrano throw some warning
 # regarding missing asset folders
 set :normalize_asset_timestamps, false
-
+set :port, 9847
 set :user, "staging"
 set :use_sudo, true
 set :keep_releases, 10
+set :default_environment, {
+  'PATH' => "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH"
+}
 
-role :web, application                   # Your HTTP server, Apache/etc
-role :app, application                   # This may be the same as your `Web` server
-role :db,  application, :primary => true # This is where Rails migrations will run
+role :web, domain                   # Your HTTP server, Apache/etc
+role :app, domain                   # This may be the same as your `Web` server
+role :db,  domain, :primary => true # This is where Rails migrations will run
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
@@ -29,12 +33,16 @@ namespace :deploy do
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
-  task :sysmlink_shared_assets do
+  task :symlink_shared_assets do
     run "ln -nfs #{shared_path}/config/mongoid.yml #{release_path}/config/mongoid.yml"
     run "ln -nfs #{shared_path}/tmp #{release_path}/tmp"
-    run "rm -nfs #{release_path}/public"
+    run "ln -nfs #{shared_path}/log #{release_path}/log"
     run "ln -nfs #{shared_path}/public #{release_path}/public"
+  end
+  task :create_db_index do
+    run "cd #{release_path} && rake db:mongoid:create_indexes RAILS_ENV=production"
   end
 end
 
 after "deploy:finalize_update", "deploy:symlink_shared_assets"
+after "deploy:finalize_update", "deploy:create_db_index"
