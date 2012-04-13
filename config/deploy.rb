2d17db51d76b84e9b4a530d4feffd6bcb8ba8693
FastND Developer <staging@FastND.Staging>
2012-04-13 16:40:07 +0000

    go to rails 3,2,2

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,7 @@ set :application, "FastND"
 set :domain, "api.finfore.net"
 set :repository,  "git@github.com:FinforeOrg/FastND.git"
 set :scm, :git
+set :branch, "staging"
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
 set :deploy_to, "/var/www/FastND"
