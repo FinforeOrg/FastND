a1a5b162b934ece4365a6f5782e6827d4e6d0eb8
Yacobus Reinhart <yacobus.reinhart@gmail.com>
2012-08-31 13:14:59 +0000

    sortable price ticker

--- app/models/price_ticker.rb
+++ app/models/price_ticker.rb
@@ -2,7 +2,11 @@ class PriceTicker
   include Mongoid::Document
   
   field :ticker, :type => String
+  field :position, :type => Integer
   index :ticker
+  index :position
+
+  default_scope asc(:position)
   
   belongs_to :feed_info, :index => true
 end
